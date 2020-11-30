5384cd5a8b169408c15a9c56c8191280ac11a4c0
Sorin Dolha <SDolha@Hotmail.com>
2020-11-30 17:11:57 +0200

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Sorin Dolha on 30.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
